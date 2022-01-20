1fa22512575d17de4adbb608f4e76c92c1b91b13
linh truong <thaolinhtruong20001905@gmail.com>
2022-01-20 16:54:32 +0700

    video 41

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,4 +1,4 @@
-import { reject } from 'bcrypt/promises';
+//import { reject } from 'bcrypt/promises';
 import userService from '../services/userService'
 let handleLogin = async (req, res) => {
     let email = req.body.email;
@@ -8,19 +8,65 @@ let handleLogin = async (req, res) => {
             errorCode: 1,
             messageCode: 'not empty input',
 
+
         })
     }
     let userData = await userService.handleUserLogin(email, password)
+    console.log('thaolinh', userData)
     return res.status(200).json({
-        error: userData.errorCode,
-        message: userData.messageCode,
+        errorCode: userData.errorCode,
+        messageCode: userData.messageCode,
         userdata: userData.user ? userData.user : { 'user': 'user not found' },
     })
 
 }
 
+let handleGetAllUsers = async (req, res) => {
+    let id = req.query.id;
+    if (!id) {
+        return res.status(200).json({
+            errorCode: 1,
+            messageCode: " Missing input",
+            users: []
+        })
+    }
+    let users = await userService.getAllUser(id);
+    //console.log(users)
+    return res.status(200).json({
+        errorCode: 0,
+        errMessage: "ok",
+        users
+    })
+
+}
+
+let handleCreateNewUser = async (req, res) => {
+    let message = await userService.createNewUser(req.body);
+
+    return res.status(200).json(message);
+}
+let handleDeleteUser = async (req, res) => {
+    if (!req.body.id) {
+        return res.status(200).json({
+            errorCode: 1,
+            messageCode: " User not found"
+        })
+
+    }
+    let message = await userService.deleteUser(req.body.id);
+    return res.status(200).json(message)
+}
+let handleEditUser = async (req, res) => {
+    let data = req.body;
+    let message = await userService.editUser(data);
+    return res.status(200).json(message)
 
 
+}
 module.exports = {
-    handleLogin: handleLogin
+    handleLogin: handleLogin,
+    handleGetAllUsers: handleGetAllUsers,
+    handleCreateNewUser: handleCreateNewUser,
+    handleEditUser: handleEditUser,
+    handleDeleteUser: handleDeleteUser
 }
